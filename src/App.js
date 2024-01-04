eadb81f7563641a1c0997c60b9da67215614c598
Abisheak1525 <abisheaksakthivelmurugan@gmail.com>
2024-01-04 12:33:14 +0530

    https://github.com/Abisheak1525/Skct_React.git

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import React from 'react';
 import CounterComponent from './Thunk/CounterComponent';
 import { Provider } from 'react-redux';
 import store from './Thunk/store';
+import Form1 from './Components/Form1';
 const App = () => {
   return (
     <div>
-      <Provider store={store}>
-        <CounterComponent/>
-      </Provider>
+      <Form1/>
     </div>
   );
 };
